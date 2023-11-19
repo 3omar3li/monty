f90b9c07c274e4b9b2d7a43cc95c263d419b0a90
3omar3li <omar.ali2332@gmail.com>
2023-11-19 14:45:44 -0800

    0. push, pall

---
+++ op_push.c
@@ -0,0 +1,70 @@
+#include "monty.h"
+
+/**
+ * push - function thats add a new node at the beginning of the stack
+ * @stack: function parameter,
+ *	the head of the stack
+ * @param: function parameter,
+ *	the value to adds on the stack
+ * Return: void
+*/
+
+void push(stack_t **stack, unsigned int param)
+{
+	stack_t *new_node = NULL;
+
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+	{
+		handle_error(ERR_BAD_MALL, NULL, 0, NULL);
+	}
+	new_node->n = param;
+	if (*stack)
+	{
+		new_node->next = *stack;
+		new_node->prev = (*stack)->prev;
+		(*stack)->prev = new_node;
+		*stack = new_node;
+		return;
+	}
+	new_node->next = *stack;
+	new_node->prev = NULL;
+	*stack = new_node;
+}
+
+/**
+ * push_queue - function thats addsa a new node at the end of the stack
+ * @stack: function parameter,
+ *	the head of the stack
+ * @param: function parameter,
+ *	the value to adds on the stack
+ * Return: void
+*/
+
+void push_queue(stack_t **stack, unsigned int param)
+{
+	stack_t *current = NULL;
+	stack_t *new_node = NULL;
+
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+	{
+		handle_error(ERR_BAD_MALL, NULL, 0, NULL);
+	}
+	new_node->n = param;
+	if (*stack)
+	{
+		current = *stack;
+		while (current->next != NULL)
+		{
+			current = current->next;
+		}
+		new_node->next = NULL;
+		new_node->prev = current;
+		current->next = new_node;
+		return;
+	}
+	new_node->next = *stack;
+	new_node->prev = NULL;
+	*stack = new_node;
+}
